e74e94a89d89f39ec13c14f4a4ffd41534da952a
ReFFToNG <49006315+ReFFToNG@users.noreply.github.com>
2024-08-19 20:45:55 +0700

    Add files via upload

--- mint-nft-lumiterra/mint.js
+++ mint-nft-lumiterra/mint.js
@@ -77,32 +77,32 @@ async function claimNFT(wallet, inviteCode) {
 
 const wallets = [
     {
-        address: '0x3050feb6757f2942bfb592e211b447c6eaad13a1',
-        privateKey: '0xe47ecbd693ce45b9fc30d57c36e09855c8eeea1e44ace441121136caf0507a10'
+        address: 'Masukan adress disini',
+        privateKey: 'Masukan Private key disini'
     },
     {
-        address: '0xd3a51665061e1424530d403aba589cb827395421',
-        privateKey: '0x602a4a312563a621016f7c10c223241a3b91e6b35d782beaaf9c7abc65e0b86d'
+        address: 'Masukan adress disini',
+        privateKey: 'Masukan Private key disini'
     },
     {
-        address: '0x018f3e2d38132938753e16d973ab4fc71abc53a8',
-        privateKey: '0x4e746a06cdb0eb4ecb7f251da8485eafe0206ef6532f93bea0a1d1ebdb6a5141'
+        address: 'Masukan adress disini',
+        privateKey: 'Masukan Private key disini'
     },
     {
-        address: '0xcc2a2dd8874dbebe21cf784fe8623e8707b2685d',
-        privateKey: '0xeee19d1cd2536fc6be4f0e6097d026d45149f16456f6d1a3a3171629bae448ff'
+        address: 'Masukan adress disini',
+        privateKey: 'Masukan Private key disini'
     },
     {
-        address: '0x1fbaf5308e025749987a5ea9c76d88fc08e68ace',
-        privateKey: '0x56a0de274915414ed3e81fc6e14daea4af520a6ae2da662ab63b8e49146de551'
+        address: 'Masukan adress disini',
+        privateKey: 'Masukan Private key disini'
     },
     {
-        address: '0xc1dbbbad6133241e3efc0bb74e8d62b639db84d1',
-        privateKey: '0x2241a0a85f3cdd13ca353272fedaef8d46a1c4c023600ffb30f303efe6879813'
+        address: 'Masukan adress disini',
+        privateKey: 'Masukan Private key disini'
     },
     {
-        address: '0x1f5243e117f38dc6ae29acefef2bbc17e5f0c72c',
-        privateKey: '0x705e4ab0e320189964d824e906af097ef674db8205a74ccabd0944cb45050fd2'
+        address: 'Masukan adress disini',
+        privateKey: 'Masukan Private key disini'
     }
 ];
 
